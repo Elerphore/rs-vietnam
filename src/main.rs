4780e41e40f1b31ac891532d74a9602c6ae33248
Eremey Malinovsky <Elerphore@gmail.com>
2022-07-31 20:13:48 +0500

    feat(): Render texture, showing mouse coords.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use macroquad::prelude::*;
 
-#[macroquad::main("BasicShapes")]
+#[macroquad::main("The thingy.")]
 async fn main() {
+
+    let vietnam_map = load_texture("img/small_asia.png").await.unwrap();
+
     loop {
-        clear_background(RED);
+        clear_background(LIGHTGRAY);
 
-        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
-        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
-        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+        draw_texture(vietnam_map,
+                     screen_width() / 2. - vietnam_map.width() / 2.,
+                     screen_height() / 2. - vietnam_map.height() / 2.,
+                     RED);
 
-        draw_text("IT WORKS!", 20.0, 20.0, 30.0, DARKGRAY);
-
+        draw_text(&*format!("{}, {}", mouse_position().0, mouse_position().1), 20.0, 20.0, 20.0, DARKGRAY);
         next_frame().await
     }
 }
