d1124d10f8a2e1565492842e84b249acedbab5f9
Eremey Malinovsky <Elerphore@gmail.com>
2022-08-01 06:25:22 +0500

    feat(): Tried to get the pixeld information with mouse coords, won't work tho.

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,31 @@ use macroquad::prelude::*;
 async fn main() {
 
     let vietnam_map = load_texture("img/small_asia.png").await.unwrap();
+    let image_itself = vietnam_map.get_texture_data();
 
     loop {
         clear_background(LIGHTGRAY);
 
+        let (mouse_x, mouse_y) = (mouse_position().0, mouse_position().1);
+        let pixel = image_itself.get_pixel(mouse_x as u32, mouse_y as u32);
+
         draw_texture(vietnam_map,
                      screen_width() / 2. - vietnam_map.width() / 2.,
                      screen_height() / 2. - vietnam_map.height() / 2.,
-                     RED);
+                     WHITE);
 
-        draw_text(&*format!("{}, {}", mouse_position().0, mouse_position().1), 20.0, 20.0, 20.0, DARKGRAY);
+        draw_text(&*format!("r:{}, g: {}, b: {}", pixel.r, pixel.b, pixel.b), 20.0, 50.0, 20.0, RED);
+        draw_text(&*format!("{:?}", Color::from_rgba(pixel.r as u8, pixel.g as u8, pixel.b as u8, pixel.a as u8)), 20.0, 70.0, 20.0, RED);
+        draw_text(&*format!("{}, {}", mouse_position().0 as u32, mouse_position().1 as u32), 20.0, 20.0, 20.0, RED);
+
+        // eprintln!("{:?}", Color::from_rgba(pixel.r as u8, pixel.g as u8, pixel.b as u8, pixel.a as u8));
+
         next_frame().await
     }
+
+    fn round(x: f32, decimals: u32) -> f32 {
+        let y = 10i32.pow(decimals) as f32;
+        (x * y).floor() / y
+    }
+
 }
